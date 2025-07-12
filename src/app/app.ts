a32857c9161ec8a44352151884438ee3b732087c
MrMoriarty04 <ramialjaber2004@gmail.com>
2025-07-12 21:57:33 +0300

    minor change

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,15 +8,62 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-password="";
 
-onButtonClick(){
-this.password="MY password";
-}
+  length=0;
+  includeLetters=false;
+  includeNumbers=false;
+  includeSymbols=false;
+  isAccepted= false;
+  password=""; 
 
-getPassword(){
-  return this.password;
-}
+
+  onChangeLength(event: Event){
+  const input = event.target as HTMLInputElement;
+  const value = parseInt(input.value);
+
+  if(!isNaN(value)){
+    this.length=value;
+  }
+
+  }
+
+  onChangeUseLetters(){
+    this.includeLetters=!this.includeLetters;
+  }
+  onChangeUseNumbers(){
+    this.includeNumbers=!this.includeNumbers;
+  }
+  onChangeUseSymbols(){
+    this.includeSymbols=!this.includeSymbols;
+  }
+
+  onButtonClick(){
+    
+    const numbers="1234567890";
+    const letters="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghigklmnopqrstuvwxyz";
+    const symbols="!@#$%^&*()_+";
+
+    let validChars="";
+    if(this.includeLetters){
+      validChars+=letters;
+    }
+    if(this.includeNumbers){
+      validChars+=numbers;
+    }
+    if(this.includeSymbols){
+      validChars+=symbols;
+    }
+
+    let generatedPassword="";
+
+    for(let i=0;i<this.length;i++){
+      const index = Math.floor(Math.random()*validChars.length);
+      generatedPassword+=validChars[index];
+    }
+
+    this.password=generatedPassword;
+  }
+
 
 
 }
